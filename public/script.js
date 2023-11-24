93f6e58e9d406c3930651f4a02ddde712f447595
Emmanuel Sarpong <83601745+emmanuelsarpong@users.noreply.github.com>
2023-11-24 12:27:50 -0500

    Changed winning score

--- public/script.js
+++ public/script.js
@@ -68,8 +68,8 @@ btnHold.addEventListener('click', function() {
     scores[activePlayer] += currentScore; 
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
-    // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
+    // 2. Check if player's score is >= 20
+    if (scores[activePlayer] >= 20) {
         // Finish game
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
         document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
